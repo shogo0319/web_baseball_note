b3a274407e5659541cd8c21bdde04f4b5457df14
shogo0319 <sho.0319bsbl@gmail.com>
2023-11-23 18:36:26 +0900

    Revert "Revert "成績入力ページ作成""
    This reverts commit a390953c20c8adc564187c93b08496dcca84d3d7.

--- resources/views/grade_create.blade.php
+++ resources/views/grade_create.blade.php
@@ -1,50 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="text-center">
-    <h3 class="my-5 text-secondary">個人成績入力フォーム</h3>
-</div>
-<form method="post" action="{#}">
-    @csrf
-    @for ($i = 1; $i <= 6; $i++)
-        <div style="display: flex; align-items: center; justify-content: center;">
-            <strong style="margin-right: 50px; font-size: 25px ">{{ $i }}打席目</strong>
-            <select class="form-select form-select-lg mb-3" style="width: 25%;" aria-label=".form-select-lg example" >
-                <option selected></option>
-                <option value="1">投</option>
-                <option value="2">捕</option>
-                <option value="3">一</option>
-                <option value="4">二</option>
-                <option value="5">三</option>
-                <option value="6">遊</option>
-                <option value="7">左</option>
-                <option value="8">中</option>
-                <option value="9">右</option>
-                <option value="10">その他</option>
-            </select>
-
-            <select class="form-select form-select-lg mb-3" style="width: 25%;" aria-label=".form-select-lg example" style="margin-left: 20px;">
-                <option selected></option>
-                <option value="1">ゴロ</option>
-                <option value="2">飛</option>
-                <option value="3">直</option>
-                <option value="4">邪飛</option>
-                <option value="5">併殺打</option>
-                <option value="6">犠打</option>
-                <option value="7">安打</option>
-                <option value="7">二塁打</option>
-                <option value="7">三塁打</option>
-                <option value="7">本塁打</option>
-                <option value="7">敵失</option>
-                <option value="7">犠飛</option>
-            </select>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="text-center">
+                <h3 class="my-5 text-secondary">個人成績入力フォーム</h3>
+            </div>
+            <form action="{{ route('grade_store') }}" method="post">
+                @csrf
+                <div class="mb-3">
+                    <label for="date" class="form-label"><strong>試合日:</strong><span class="badge bg-danger">必須</label>
+                    <input type="date" name="date" id="date" class="form-control @error('date') is-invalid @enderror" value="{{date('Y-m-d')}}">
+                    @error('title')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="opponent" class="form-label"><strong>対戦相手:</strong><span class="badge bg-danger">必須</label>
+                    <input type="text" name="opponent" id="opponent" class="form-control @error('opponent') is-invalid @enderror">
+                    @error('opponent')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    @for ($i = 1; $i <= $numberOfAtBats; $i++)
+                        <label for="grade" class="form-label"><strong>{{ $i }}打席目:</strong><span class="badge bg-secondary">任意</span></label><br>
+                        <select name="position_{{ $i }}" id="position_{{ $i }}" style="width: 50%;" class="mb-3">
+                            <option value="" disabled selected>打球方向を選択してください</option>
+                            @foreach ($positions as $key => $position)
+                                <option value="{{ $key }}">{{ $position }}</option>
+                            @endforeach
+                        </select>
+                        <select name="result_{{ $i }}" id="result_{{ $i }}" style="width: 49%;">
+                            <option value="" disabled selected>打撃結果を選択してください</option>
+                            @foreach($results as $key => $result)
+                                <option value="{{ $key }}">{{ $result }}</option>
+                            @endforeach
+                        </select>
+                    @endfor
+                </div>
+                <div class="mb-5">
+                    <label for="rbi" class="form-label"><strong>打点:</strong><span class="badge bg-danger">必須</label>
+                    <input type="number" name="rbi" id="rbi" class="form-control @error('rbi') is-invalid @enderror" >
+                    @error('rbi')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="text-center">
+                    <input type="submit" value="送信" class="btn btn-primary">
+                </div>
+            </form>
         </div>
-    @endfor
-<div style="display: flex; align-items: center; justify-content: center;">
-    <strong style="margin-right: 50px; font-size: 25px">打点</strong>
-    <input type="number" name="rbi" class="form-control" style="width: 25%;" placeholder="打点を入力">
-</div>
-<div class="text-center mt-4">
-    <button type="submit" class="btn btn-primary">送信</button>
+    </div>
 </div>
 @endsection
