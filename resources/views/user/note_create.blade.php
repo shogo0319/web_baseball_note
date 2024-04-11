637fa65e7a932518004e418460d02845953f2092
shogo0319 <sho.0319bsbl@gmail.com>
2024-04-12 03:13:06 +0900

    選手ノート作成ビュー修正

--- resources/views/user/note_create.blade.php
+++ resources/views/user/note_create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container col-md-8">
     <div class="text-center">
-        <h3 class="my-5 text-secondary">ノート作成フォーム</h3>
+        <h2 class="my-5 text-secondary">ノート作成フォーム</h2>
     </div>
     <form action="{{ route('user.note_store') }}" method="post" novalidate>
         @csrf
@@ -15,14 +15,14 @@
             @enderror
         </div>
         <div class="mb-3">
-            <label for="swing" class="form-label"><strong>素振り回数 </strong><span class="badge bg-danger">必須</label>
-            <input type="number" class="form-control @error('swing') is-invalid @enderror" id="swing" name="swing" value="{{ old('swing') }}" placeholder="50" required>
+            <label for="swing" class="form-label"><strong>素振り回数（回） </strong><span class="badge bg-danger">必須</label>
+            <input type="number" class="form-control @error('swing') is-invalid @enderror" id="swing" name="swing" value="{{ old('swing') }}" placeholder="50" min="0" required>
             @error('swing')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
         <div class="mb-3">
-            <label for="running" class="form-label"><strong>ランニング距離(km) </strong><span class="badge bg-danger">必須</label>
+            <label for="running" class="form-label"><strong>ランニング距離（km） </strong><span class="badge bg-danger">必須</label>
             <input type="number" class="form-control @error('running') is-invalid @enderror" id="running" name="running" value="{{ old('running') }}" placeholder="3" step="0.1" required>
             @error('running')
                 <div class="alert alert-danger">{{ $message }}</div>
@@ -33,12 +33,12 @@
             <input type="text" class="form-control" id="practice" name="practice" value="{{ old('practice') }}" placeholder="キャッチボール、ノック、シートバッティング...">
         </div>
         <div class="mb-3">
-            <label for="youtube_link" class="form-label"><strong>YouTubeリンク </strong><span class="badge bg-secondary">任意</span></label>
+            <label for="youtube_link" class="form-label"><strong>参考にしたYouTubeのリンク </strong><span class="badge bg-secondary">任意</span></label>
             <input type="url" class="form-control" id="youtube_link" name="youtube_link" value="{{ old('youtube_link') }}" placeholder="https://www.xxx.baseball_note.jp">
         </div>
         <div class="mb-3">
-            <label for="weight" class="form-label"><strong>体重(kg) </strong><span class="badge bg-danger">必須</label>
-            <input type="number" class="form-control @error('weight') is-invalid @enderror" id="weight" name="weight" value="{{ old('weight') }}" placeholder="70.5" step="0.1" required>
+            <label for="weight" class="form-label"><strong>体重（kg） </strong><span class="badge bg-danger">必須</label>
+            <input type="number" class="form-control @error('weight') is-invalid @enderror" id="weight" name="weight" value="{{ old('weight') }}" placeholder="60.5" step="0.1" min="0" required>
             @error('weight')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
@@ -58,13 +58,13 @@
             @enderror
         </div>
         <div class="mb-3">
-            <label for="memo" class="form-label"><strong>今日の振り返り（気づきや学び、感想など自由に記述）</strong><span class="badge bg-danger">必須</label>
-            <textarea class="form-control @error('memo') is-invalid @enderror" id="memo" name="memo" value="{{ old('memo') }}" rows="3" required></textarea>
+            <label for="memo" class="form-label"><strong>今日の振り返り（気づきや学び、感想など自由に記述）</strong><span class="badge bg-danger">必須</span></label>
+            <textarea class="form-control @error('memo') is-invalid @enderror" id="memo" name="memo" rows="3" required>{{ old('memo') }} </textarea>
             @error('memo')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
-        <div class="text-center mt-5 mb-3">
+        <div class="text-center my-5">
             <input class="btn btn-outline-success" type="submit" value="作成する">
             <a href="{{ route('user.notes_index') }}" class="btn btn-outline-secondary">一覧に戻る</a>
         </div>
