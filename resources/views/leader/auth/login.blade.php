a6320d4108f37280bdc4b3edfe044ac99fdfb98d
shogo0319 <sho.0319bsbl@gmail.com>
2024-04-11 15:53:51 +0900

    選手指導者のログイン画面修正

--- resources/views/leader/auth/login.blade.php
+++ resources/views/leader/auth/login.blade.php
@@ -5,21 +5,19 @@
     <div class="row justify-content-center align-items-center" style="height: 90vh;">
         <div class="col-md-6">
             <div class="text-center mb-5" style="color: #D3D3D3;">
-                <h1><strong> 野 球 ノ ー ト</strong></h1>
+                <h1><strong>WEB BASEBALL NOTE</strong></h1>
             </div>
             <div class="card border-dark" style="background-color: rgba(0, 0, 0, 0.5); color:#D3D3D3; font-size: 1.3em; border-radius: 30px;">
                 <div class="card-header text-center">
-                    <strong>ログイン</strong>
+                    <strong>指導者ログイン</strong>
                 </div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('leader.login.login') }}">
                         @csrf
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('メールアドレス') }}</label>
-
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus style="background-color: rgba(0, 0, 0, 0); color: #D3D3D3; border: 1px solid #D3D3D3;">
-
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -27,13 +25,10 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('パスワード') }}</label>
-
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" style="background-color: rgba(0, 0, 0, 0); color: #D3D3D3; border: 1px solid #D3D3D3;">
-
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -41,30 +36,12 @@
                                 @enderror
                             </div>
                         </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }} style="background-color: rgba(0, 0, 0, 0); color: #D3D3D3; border: 1px solid #D3D3D3;">
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('ログイン状態を保持する') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('ログイン') }}
                                 </button>
                                 <a href="{{ route('leader.create') }}" class="btn btn btn-success">{{ __('新規登録はこちら') }}</a>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}" style="color: #D3D3D3;">
-                                        {{ __('パスワードを忘れた方はこちら') }}
-                                    </a>
-                                @endif
                             </div>
                         </div>
                     </form>
